e027c7b77bd1738ebd2d9bf0ac4362767ee17f73
Dawid Walicki <dwalicki.ai@gmail.com>
2023-11-02 21:10:20 +0100

    Add files via upload

--- helper_functions.py
+++ helper_functions.py
@@ -29,6 +29,8 @@ from torch import nn
 import torchvision.models as models
 from torchvision.models import resnet50, ResNet50_Weights
 from torch.optim.lr_scheduler import StepLR
+import pytorch_ssim
+from pytorch_ssim import ssim
 
 def count_files_in_drive(folder_file_count: dict, zoom: int, DRIVE_PATH: str):
   '''Function that returns number of files in drive path'''
@@ -48,7 +50,7 @@ def count_files_in_drive(folder_file_count: dict, zoom: int, DRIVE_PATH: str):
 
   return folder_file_count
 
-def create_dataset(PATH: str, data: pd.DataFrame, output_property: str):
+def create_dataset(PATH: str, zoom: int, data: pd.DataFrame, output_property: str):
   '''This function creates dataset - each sample with different magnifications is
       connected with chosen property'''
 
@@ -56,8 +58,10 @@ def create_dataset(PATH: str, data: pd.DataFrame, output_property: str):
   properties = []
   samples = []
   for root, dirs, files in os.walk(PATH):
+    folder_name = os.path.basename(root)
     for file in files:
-      if file.endswith('.jpg'):
+      filename = folder_name+'_'+str(zoom)
+      if file.startswith(filename) & file.endswith('.jpg'):
         image_path = os.path.join(root, file)
         folder_name = os.path.basename(os.path.dirname(image_path))
         try:
@@ -168,6 +172,7 @@ class CustomImageDataset(torch.utils.data.Dataset):
 def train_step(model: torch.nn.Module,
                train_dataloader: torch.utils.data.DataLoader,
                loss_fn: torch.nn.Module,
+               loss_ssim: bool,
                optimizer: torch.optim.Optimizer,
                device: torch.device):
   train_loss, train_metric = 0, 0
@@ -187,7 +192,10 @@ def train_step(model: torch.nn.Module,
     #Calculate loss
     # print(f'Wymiary y_pred: {y_pred.shape}')
     # print(f'Wymiary y: {y.shape}')
-    loss = loss_fn(y_pred, y) #było y.squeeze()
+    if loss_ssim:
+      loss = 1 - ssim(y_pred, y)
+    else:
+      loss = loss_fn(y_pred, y) #było y.squeeze()
     train_loss += loss.item()
 
     #Optimizer zero grad
